d56797f851e861d754ed1e0ec3220372cabc8f8e
GerardoCruz163 <gerarojo243@gmail.com>
2025-04-05 10:33:48 -0600

    cambio al toolbar de summernote

--- view/DetalleTicket/detalleticket.js
+++ view/DetalleTicket/detalleticket.js
@@ -20,14 +20,31 @@ $(document).ready(function(){
             onPaste: function (e) {
                 console.log("Text detect...");
             }
-        }
+        },
+        toolbar: [
+            // [groupName, [list of button]]
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+          ]
     
     });
 
     $('#tickd_descripusu').summernote({
         height: 250,
-        lang: "es-ES"
-    
+        lang: "es-ES",
+        toolbar: [
+            // [groupName, [list of button]]
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['font', ['strikethrough', 'superscript', 'subscript']],
+            ['fontsize', ['fontsize']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['height', ['height']]
+          ]
     });
 
     $('#tickd_descripusu').summernote('disable');
