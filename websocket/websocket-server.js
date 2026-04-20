0a3edb2deb1322cfdc909276b0391576497908b0
GerardoCruz163 <gerarojo243@gmail.com>
2026-04-20 12:54:21 -0500

    Prueba para Notificaciones Push en detalleticket y websocket-server

--- websocket/websocket-server.js
+++ websocket/websocket-server.js
@@ -78,7 +78,7 @@
 // });
 
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const socketio = require('socket.io');
 const fs =  require('fs');
 const app = express();
