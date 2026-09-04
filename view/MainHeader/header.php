137c28cb833d67db1c33173c705c695076bcc42f
TLA-SISTEMAS-GERA <sistemas@tecnologisticaaduanal.com>
2026-09-04 09:58:42 -0500

    logo septiembre

--- view/MainHeader/header.php
+++ view/MainHeader/header.php
@@ -8,7 +8,7 @@
 
         <a href="../Home/index.php" class="site-logo">
             <img class="hidden-md-down" src="../../public/img/SUTRAT.png" alt="">
-            <img class="hidden-lg-up" src="../../public/img/SUTRA REDT.png" alt="">
+            <img class="hidden-lg-up" src="../../public/img/SUTRASEPT.GIF" alt="">
         </a>
 
         <button id="show-hide-sidebar-toggle" class="show-hide-sidebar">
